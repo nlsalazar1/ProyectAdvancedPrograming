90b11c84d8a3fd0faa173dad472775cc08f39698
Vicente Xavier Vaca Perez <32905919+vxvaca1@users.noreply.github.com>
2019-06-10 12:01:44 -0500

    Operation

--- Restaurant APP/U_10.js
+++ Restaurant APP/U_10.js
@@ -1,1 +1,10 @@
-Operacion 
+function operation(){
+    var name = document.getElementById('name').value;
+    if (name === "") {
+        alert('MUST GIVE AN ID');
+        return false;
+    } else{        
+        document.getElementById('form').action = "./U_10.php";        
+        return true;
+    }
+}
