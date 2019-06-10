e9ac0b2684e9882f889f50f56dfdb9719fdd371f
Salazar Guerra Ney Leonardo <49221143+nlsalazar1@users.noreply.github.com>
2019-06-10 11:59:05 -0500

    MENU

--- Restaurant APP/U_17.js
+++ Restaurant APP/U_17.js
@@ -1,10 +1,1 @@
-function operation(){
-    var nameSaucer = document.getElementById('nameSaucer').value;
-    if (nameSaucer === "") {
-        alert('MUST GIVE AN ID');
-        return false;
-    } else{        
-        document.getElementById('form').action = "./U_17.php";        
-        return true;
-    }
-}
+D
