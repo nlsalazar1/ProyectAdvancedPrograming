e5897876a18578a51c0e94f53568f66b0142024f
Vicente Xavier Vaca Perez <32905919+vxvaca1@users.noreply.github.com>
2019-06-10 12:11:28 -0500

    Operation

--- Restaurant APP/U_12.js
+++ Restaurant APP/U_12.js
@@ -1,12 +1,13 @@
 
-function buy(){
-    var nameSaucer = document.getElementById('nameSaucer').value;
-    var qualification = document.getElementById('qualification').value;
-    if (nameSaucer === "" || qualification === "") {
+function operation(){
+    var name = document.getElementById('name').value;
+    var lastname = document.getElementById('lastname').value;
+    if (name === "" || lastname === "") {
         alert('MUST GIVE AN ID');
         return false;
     } else{        
-        document.getElementById('form').action = "./buy.php";        
+        document.getElementById('form').action = "./U_12.php";        
         return true;
     }
 }
+
