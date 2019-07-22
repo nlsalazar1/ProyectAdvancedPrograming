17a439b1ebb9ccd48822be9d452ed4702faccf99
Luigi Villarreal Campaña <46844194+LuigiVillarreal@users.noreply.github.com>
2019-07-22 13:15:35 -0500

    Update Conexion.java

--- CyberRestaurant/src/java/conexion/Conexion.java
+++ CyberRestaurant/src/java/conexion/Conexion.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class Conexion {
 
     private static final String db = "restaurant";
-    private static final String URL = "jdbc:mysql://localhost:3306/" + db;
+    private static final String URL = "jdbc:mysql://instanciamdb1.c8t145bpfcn2.us-east-1.rds.amazonaws.com:3306/" + db;
     private static final String USERNAME = "root";
-    private static final String PASSSWORD = "admin";
+    private static final String PASSSWORD = "espe2017";
 
     public  Connection getConnection() {
         Connection connection = null;
